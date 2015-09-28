e14797cfc72b9904d6e9ffe31bf55d031d4e7e82
Jen Wachter <jenleighkelly@gmail.com>
2015-09-28 13:12:36 -0400

    Inial commit

---
+++ src/Logger.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Logger;
+
+class Logger
+{
+	protected $niceName;
+	protected $machineName;
+	protected $logfile;
+
+	public function __construct($niceName, $machineName, $localFile, $stagingFile, $productionFile)
+	{
+		$this->niceName = $niceName;
+		$this->machineName = $machineName;
+		$this->logger = new \Monolog\Logger($this->machineName);
+
+		if (ENV == "local") {
+			$this->logfile = $localFile;
+		} else if (ENV == "staging") {
+			$this->logfile = $stagingFile;
+		} else {
+			$this->logfile = $productionFile;
+		}
+
+		$this->setupStreamHandler();
+
+		if (ENV != "local") {
+			$this->setupEmailHandler();
+		}
+	}
+
+  protected function get()
+  {
+    return new \LoggerExchange\adapters\Monolog($this->logger);
+  }
+
+	protected function setupStreamHandler()
+	{
+		// make sure file is writable before creating handler
+		if (is_writable($this->logfile)) {
+			$handler = new \Monolog\Handler\StreamHandler($this->logfile, \Monolog\Logger::DEBUG);
+			$this->logger->pushHandler($handler);
+		}
+	}
+
+	protected function setupEmailHandler()
+	{
+		$transport = \Swift_MailTransport::newInstance();
+		$mailer = \Swift_Mailer::newInstance($transport);
+		$message = \Swift_Message::newInstance()
+			->setSubject("Error on {$this->niceName}")
+			->setFrom(array("noreply@jhu.edu" => "jhu.edu"))
+			->setTo(array("jwachter@jhu.edu", "jenleighkelly@gmail.com"));
+
+		$this->logger->pushHandler(new \Monolog\Handler\SwiftMailerHandler($mailer, $message, \Monolog\Logger::WARNING));
+	}
+
+}
