e5136e2f05270f7e4642b5d68d72ea54a1a0c881
Jen Wachter <jwachter@jhu.edu>
2022-05-26 12:35:57 -0400

    updated sentry logger to work with current SDK -- closes #1

--- src/Adapters/Sentry.php
+++ src/Adapters/Sentry.php
@@ -6,56 +6,66 @@ use Logger\Interfaces\Logger;
 
 class Sentry implements Logger
 {
-  public function __construct($client)
+  public function __construct($dsn, $options = [])
   {
-    $this->client = $client;
+    \Sentry\init(array_merge($options, ['dsn' => $dsn]));
   }
 
   /**
    * Log a message to sentry
    * @param  string $level   Message level (debug, info, warning, error, fatal)
    * @param  string $message Message to log
-   * @param  array  $data    Additional logging data (key => value)
+   * @param  array  $options Additional data to send to Sentry (tags, context, etc...)
    * @return null
    */
-  protected function log($level, $message, array $data = [])
+  protected function log(string $level = 'debug', string $message = '', array $data = [])
   {
-    $compiled = ['level' => $level];
+    \Sentry\withScope(function (\Sentry\State\Scope $scope) use ($level, $message, $data): void {
 
-    if (isset($data['tags'])) {
-      $compiled['tags'] = $data['tags'];
-      unset($data['tags']);
-    }
+      $scope->setLevel(\Sentry\Severity::$level());
 
-    // // throw the rest into extra
-    // $compiled['extra'] = $data;
+      if (isset($data['context']) && is_array($data['context'])) {
+        // note: that data WILL get clipped, so limit what you send to relevant data only
+        // docs: https://docs.sentry.io/platforms/php/enriching-events/context/#size-limitations
+        $scope->setContext('app context', $data['context']);
+      }
 
-    return $this->client->captureMessage($message, $data, $compiled);
+      if (isset($data['tags']) && is_array($data['context'])) {
+        // examples: wp.theme, wp.plugin
+        // docs: https://docs.sentry.io/platforms/php/enriching-events/tags/
+        foreach ($data['tags'] as $key => $value) {
+          $scope->setTag($key, $value);
+        }
+      }
+
+      \Sentry\captureMessage($message);
+
+    });
   }
 
   public function addDebug($message, array $data = [])
   {
-    return $this->log("debug", $message, $data);
+    return $this->log('debug', $message, $data);
   }
 
   public function addInfo($message, array $data = [])
   {
-    return $this->log("info", $message, $data);
+    return $this->log('info', $message, $data);
   }
 
   public function addWarning($message, array $data = [])
   {
-    return $this->log("warning", $message, $data);
+    return $this->log('warning', $message, $data);
   }
 
   public function addError($message, array $data = [])
   {
-    return $this->log("error", $message, $data);
+    return $this->log('error', $message, $data);
   }
 
   public function addFatal($message, array $data = [])
   {
-    return $this->log("fatal", $message, $data);
+    return $this->log('fatal', $message, $data);
   }
 
 }
