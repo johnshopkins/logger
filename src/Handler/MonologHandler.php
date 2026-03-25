f94279decc1f37cfadfcd476a2ef7b6ce24673b7
Jen Wachter <jwachter@jhu.edu>
2026-03-25 17:17:48 -0400

    Removed is_writeable() check in monolog handler

--- src/Handler/MonologHandler.php
+++ src/Handler/MonologHandler.php
@@ -9,11 +9,8 @@ class MonologHandler implements HandlerInterface
   public function __construct($loggerName, $logFile)
   {
     $this->client = new \Monolog\Logger($loggerName);
-
-    if (is_writable($logFile)) {
-      $handler = new \Monolog\Handler\StreamHandler($logFile);
-      $this->client->pushHandler($handler);
-    }
+    $handler = new \Monolog\Handler\StreamHandler($logFile);
+    $this->client->pushHandler($handler);
   }
 
   public function handle(string $level, string $message, array $context = []): void
